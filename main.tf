01bef1d45b1c152f7df55a9eab1715b059ee6c3b
Kortans <kortansyolo@gmail.com>
2024-10-02 16:58:42 +0200

    Ajout de la référence d'un VPC

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+#--------------------Provider----------------------
 provider "google" {
     project = var.project_id
     credentials = "./cred.json"
-    region  = var.region
+    region  = europe-west9
+}
+
+#--------------------VPC---------------------------
+resource "google_compute_network" "vpc_network" {
+  project                 = "VCC-TD2-terraform"
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
 }
